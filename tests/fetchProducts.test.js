598d62b9295b46d705f292d74c7fb73eef9aadc5
Paulo Porto <prtpj1@gmail.com>
2022-02-08 00:36:26 -0300

    Req 8 - 2 of 5 tests working

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -2,7 +2,17 @@ require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
-describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+describe('1 - Test the function fecthProducts', () => {
+  it('Checks if fetchProducts is a function',
+    async () => {
+      expect(typeof fetchProducts).toBe('function');
+    });
+  it('Check if when called fetchProducts(\'computador\') fetch returns', 
+  async () => {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=computador`;
+    await fetchProducts('computador');
+
+    expect(fetch).toHaveBeenCalledWith(url);
+  })
+  // fail('Teste vazio');
 });
