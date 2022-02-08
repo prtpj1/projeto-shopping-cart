3a7d1cc273cf81567532a11296ca5e152cd14dc4
Paulo Porto <prtpj1@gmail.com>
2022-02-08 00:09:02 -0300

    Req 1 fetchProducts is working

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,9 +1,27 @@
-const fetchProducts = () => {
-  // seu código aqui
-};
+const fetchProducts = async () => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=computador`;
 
+  const response = await fetch(url);
+  const data = await response.json();
+  // console.log(data.results);
+  return data.results;
+
+};
+// fetchProducts();
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,
   };
 }
+
+/*
+Criar uma listagem de produtos retirados da API do ML
+A listagem terá seus elementos HTML criados na função createProductItemElement() em 
+script.js
+O elemento criado pela função createProductItemElement() deve ser filho de <section class="items">
+
+O endpoint: "https://api.mercadolibre.com/sites/MLB/search?q=computador"
+O retorno do endpoint virá no formato json
+A lista de produtos exibidos é um array do json no readme
+fetchProducts precisa ser chamada no arquivo script.js
+*/
