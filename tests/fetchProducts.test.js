eda1db8c1021f79cae132729b082fc4761e751da
Paulo Porto <prtpj1@gmail.com>
2022-02-08 23:12:30 -0300

    Finish all 5 tests of req 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -7,12 +7,29 @@ describe('1 - Test the function fecthProducts', () => {
     async () => {
       expect(typeof fetchProducts).toBe('function');
     });
-  it('Check if when called fetchProducts(\'computador\') fetch returns', 
-  async () => {
-    const url = `https://api.mercadolibre.com/sites/MLB/search?q=computador`;
-    await fetchProducts('computador');
+  it('Check if when called fetchProducts(\'computador\') fetch returns',
+    async () => {
+      await fetchProducts('computador');
 
-    expect(fetch).toHaveBeenCalledWith(url);
-  })
+      expect(fetch).toHaveBeenCalled();
+    });
+  it('Test if fetch use the correct endpoint URL when the function fetchProducts(\'computador\') is called',
+    async () => {
+      await fetchProducts('computador');
+
+      expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+    });
+    it('Test if the function fetchProducts(\'computador\') return is a data structure equal to object computadorSearch been imported',
+    async () => {
+      const products = await fetchProducts('computador');
+
+      expect(products).toMatchObject(computadorSearch);
+    });
+    it('Test if the function fetchProducts called without parameters return an error message: \'You must provide an url\'',
+    async () => {
+      const noProducts = await fetchProducts();
+      
+      expect(noProducts).toEqual(new Error('You must provide an url'));
+    });
   // fail('Teste vazio');
 });
